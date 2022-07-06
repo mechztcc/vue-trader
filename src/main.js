3c70645165d28af971dc9ec723d383dfef92cf05
mechztcc <dojathan@gmail.com>
2022-07-06 10:36:53 -0400

    feat: add vuetify

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import vuetify from './plugins/vuetify'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app');
+
+new Vue({
+  router,
+  vuetify,
+
+  render(h) {
+    h(App);
+  }
+}).$mount('#app');
